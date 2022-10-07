d56b311d61ee34ee23aaedaf50b6bc5f7634dda8
leonardo-lin <st950065@yahoo.com.tw>
2022-10-07 19:43:01 +0800

    creat hw4.py

---
+++ hw4/hw4.py
@@ -0,0 +1,16 @@
+from math import ceil
+
+
+print("hi there")
+import math
+def sol():
+    n=int(input("n= "))
+    F=int(input("F= "))
+    sqF=F**2
+    print(sqF)
+    print(math.ceil(sqF))
+
+    pass
+
+if __name__ == "__main__":
+    sol()
